7c9718e6bc4c1e4b6adc3b5fb9b09ce4473b6913
Nyxeel <nyxeel@protonmail.com>
2026-02-06 20:41:18 +0100

    ADDED: cleanup_mlx if load textures in init.c fails

--- src/render/init.c
+++ src/render/init.c
@@ -22,27 +22,26 @@ static bool	load_textures(t_data *game)
 
 static bool	init_mlx(t_data *game)
 {
-
 	game->mlx = mlx_init();
 	if (!game->mlx)
 		return (print_error("mlx_init failed\n", game, 0), false);
 	if (!load_textures(game))
-	{
-		mlx_destroy_display(game->mlx);
-		free(game->mlx);
-		return (print_error("loading textures failed\n", game, 0), false);
-	}
+		return (clean_up_mlx(t_data *game), \
+			print_error("loading textures failed\n", game, 0), false);
 	game->mlx_win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "cub3D");
 	if (!game->mlx_win)
-		return (clean_up_mlx(game), print_error("mlx_new_window failed\n", game, 0), false);
+		return (clean_up_mlx(game), \
+			print_error("mlx_new_window failed\n", game, 0), false);
 	game->image->img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
 	if (!game->image->img)
-		return (clean_up_mlx(game), print_error("mlx_new_image failed\n", game, 0), false);
+		return (clean_up_mlx(game), \
+			print_error("mlx_new_image failed\n", game, 0), false);
 	game->image->addr = mlx_get_data_addr(game->image->img,
 		&game->image->bits_per_pixel, &game->image->line_length,
 		&game->image->endian);
 	if (!game->image->addr)
-		return (clean_up_mlx(game), print_error("mlx_get_data_addr failed\n", game, 0), false);
+		return (clean_up_mlx(game), \
+			print_error("mlx_get_data_addr failed\n", game, 0), false);
 	return (true);
 }
 
